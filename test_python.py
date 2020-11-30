13ba77772c3b460a0ca235af10993162e8ba6599
eran edri <eranedri@h-MacBook-Pro-sl-Roni.local>
2020-11-30 16:28:27 +0200

    push

---
+++ test_python.py
@@ -0,0 +1,18 @@
+import requests
+import ndjson
+import json
+import datetime
+from datetime import date
+# import pandas_gbq as pg
+import pandas as pd
+import numpy as np
+import time
+# import mysql.connector
+from pandas import json_normalize
+# from sqlalchemy import create_engine
+import csv
+
+
+
+if __name__ == '__main__':  
+  print("edri")
