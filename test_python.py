7face571bbd6babd0c356b25ef2af48f5dd1eee3
eran edri <eranedri@h-MacBook-Pro-sl-Roni.local>
2020-11-30 16:53:51 +0200

    push

--- test_python.py
+++ test_python.py
@@ -1,18 +1,31 @@
 import requests
-import ndjson
 import json
 import datetime
 from datetime import date
-# import pandas_gbq as pg
 import pandas as pd
 import numpy as np
 import time
-# import mysql.connector
-from pandas import json_normalize
-# from sqlalchemy import create_engine
 import csv
+import os
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 
+credentials = service_account.Credentials.from_service_account_file('path/to/file.json')
 
-if __name__ == '__main__':  
-  print("edri")
+project_id = 'my-bq'
+client = bigquery.Client(credentials=credentials, project=project_id)
+
+QUERY = (
+    'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
+    'WHERE state = "TX" '
+    'LIMIT 100')
+query_job = client.query(QUERY)
+rows = query_job.result()
+
+for row in rows:
+    print(row.name)
+
+
+# if __name__ == '__main__':
+#     print("edri")
